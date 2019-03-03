7e035f78c426f933f046e4ed3a52e12e786ad4f8
Giuseppe Di Giorno <giuseppedigiorno@yahoo.it>
2019-03-03 16:53:00 +0100

    Risolto lag dovuto ad erroneo caricamento font

--- src/shared/css/fonts.js
+++
@@ -1,4 +0,0 @@
-import MontserratRegular from "./montserrat-v12-latin-regular.woff2";
-import MontserratBold from "./montserrat-v12-latin-800.woff2";
-
-export { MontserratRegular, MontserratBold };
