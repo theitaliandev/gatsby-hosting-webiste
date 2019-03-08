6d5f0ba5c8fba8c7e869162212fa1e279ee1000c
Giuseppe Di Giorno <giuseppedigiorno@yahoo.it>
2019-03-08 18:51:50 +0100

    Fix pagination

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `posts`,
-        path: `${__dirname}/src/posts`
+        path: `${__dirname}/src/pages/blog/posts`
       }
     },
     `gatsby-transformer-sharp`,
--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import Layout from "../components/layout";
-import BlogList from "../components/blogListTemplate";
+import Layout from "../../components/layout";
+import BlogList from "../../components/blogListTemplate";
 
 const Blog = () => {
   return (
