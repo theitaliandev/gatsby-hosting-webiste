95c4d2ddf8483d46f5b881d67ea6033b2946b7f9
Giuseppe Di Giorno <giuseppedigiorno@yahoo.it>
2019-08-29 22:50:17 +0200

    Aggiunta sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`
+    title: `Migliori Hosting per Wordpress`,
+    description: `Abbiamo testato e recensito i migliori hosting per Wordpress.`,
+    author: `@neunet`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -23,6 +23,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     "gatsby-plugin-styled-components",
+    "gatsby-plugin-sitemap",
     "gatsby-transformer-remark",
     {
       resolve: `gatsby-plugin-manifest`,
