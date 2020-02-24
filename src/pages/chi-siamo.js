7414c9b27999d7a0db0dbd7b381249f671685e75
Giuseppe Di Giorno <giuseppedigiorno@yahoo.it>
2020-02-24 18:23:47 +0100

    Update chi-siamo.js

--- src/pages/chi-siamo.js
+++ src/pages/chi-siamo.js
@@ -12,13 +12,12 @@ const ChiSiamo = () => (
       <b>Besty5 Internet Solutions</b> è una società di appassionati per
       appassionati. A guidarci è la passione per il mondo di Internet e per la
       tecnologia in toto, passione che ci ha portati ad entrare nel mondo dei
-      servizi per il web, da chi nasce il sito che stai visitando e da cui, in
-      passato, è nato il nostro canale di punta:{" "}
-      <a href="https://www.besty5.com">www.besty5.com</a>
+      servizi per il web, da cui nasce il sito che stai visitando e da cui, in
+      passato, sono nati molti altri portali.
     </p>
     <br />
     <p>
-      Non solo besty5.com, ma diversi altri siti del nostro network ci hanno
+      Tutti i siti del nostro network ci hanno
       permesso di maturare l’esperienza necessaria a realizzare NeuWP,{" "}
       <b>la miglior guida agli hosting Wordpress che tu possa desiderare!</b>
     </p>
